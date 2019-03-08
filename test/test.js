46bc470f5a8f5e87ddfe1900d70b752725fdbd35
shorouq <shrooqabdullahsaad@gmail.com>
2019-03-08 17:07:49 +0200

    add home router

--- test/test.js
+++ test/test.js
@@ -4,6 +4,18 @@ const app = require('../src/app');
 const built = require('../src/database/db_built');
 
 tape('test', (test) => {
-  test.equal(1,1,'must equal');
+  test.equal(1, 1, 'must equal');
   test.end();
+});
+
+tape('home page', test => {
+  supertest(app)
+    .get('/')
+    .expect(200)
+    .expect('content-type', 'text/html; charset=utf-8')
+    .end((err, res) => {
+      if (err) test.error(err);
+      test.equal(res.text.includes('<title>Posts</title>'), true, 'pass the test of home page');
+      test.end()
+    });
 });
