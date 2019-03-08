4df0ada88527aa3f8deeb1ad0bc95576ff6c7f0e
shorouq <shrooqabdullahsaad@gmail.com>
2019-03-08 16:09:16 +0200

    enable built

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 const tape = require('tape');
 const supertest = require('supertest');
-const app = require('../src/index');
-// const built = require('../src/database/db_built');
+const built = require('../src/database/db_built');
 tape('test', (test) => {
   test.equal(1,1,'must equal');
   test.end();
