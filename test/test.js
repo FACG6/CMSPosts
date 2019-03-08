cd964176ca59f3c82351709a62dd69748da0d349
shorouq <shrooqabdullahsaad@gmail.com>
2019-03-08 16:16:11 +0200

    enable built and run it

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 const tape = require('tape');
 const supertest = require('supertest');
+const app = require('../src/app');
 const built = require('../src/database/db_built');
+
 tape('test', (test) => {
+  built()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
   test.equal(1,1,'must equal');
   test.end();
 });
